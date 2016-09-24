77e449613970227daf1b4bfd8631d20568d38a33
Rahul <rahul.nambiar@gatech.edu>
2016-09-24 08:47:28 -0400

    removed unnecessary files

---
+++ Divvy/HomePageView.swift
@@ -0,0 +1,15 @@
+//
+//  HomePageView.swift
+//  Divvy
+//
+//  Created by Rahul Nambiar on 9/24/16.
+//  Copyright © 2016 Sahaj Bhatt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class HomePageView: UIView {
+    
+}
