87fbf62ef1ab5298ca9fd4345ac3259cf85c5356
[Karan Lakhani] <karanlakhani17@gmail.com>
2016-09-24 14:18:21 -0400

    Added camera feature

--- Divvy/HomePageView.swift
+++ Divvy/HomePageView.swift
@@ -10,11 +10,15 @@ import Foundation
 import UIKit
 
 
-class HomePageView: UIView {
+class HomePageView: UIView, UITextViewDelegate {
     
+    //Buttons
+    var camera = UIButton()
     
     func configureButtons() {
-        
+        camera.setTitle("Camera", forState: .Normal)
+        camera.backgroundColor = DVUIConstants.colors.loginPeach
+        camera.setTitleColor(UIColor.whiteColor(), forState: .Normal)
     }
     
     func configureLogo() {
@@ -22,15 +26,34 @@ class HomePageView: UIView {
     }
     
     func configureViews() {
+
+
+    }
+    func configureView() {
+        self.backgroundColor = UIColor.clearColor()
         
+        configureButtons()
+        
+        
+        let viewsDict = [
+            
+            "camera":   camera,
+            
+            ]
+        
+        self.prepareViewsForAutoLayout(viewsDict)
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-40-[camera(==\(String(DVUIConstants.cameraHeight)))]-25-|", views: viewsDict))
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-15-[camera]-15-|", views: viewsDict))
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configureViews()
+        configureView()
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 }
---
+++ Divvy/TransactionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TransactionViewController.swift
+//  Divvy
+//
+//  Created by Karan Lakhani on 9/24/16.
+//  Copyright © 2016 Sahaj Bhatt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TransactionViewController: UIViewController {
+    
+}
