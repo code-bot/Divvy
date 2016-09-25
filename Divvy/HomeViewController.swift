1d235993a985ace23496a773c5dec2c9ed26d9db
Sahaj Bhatt <sahajb97@gmail.com>
2016-09-25 01:39:08 -0400

    Commented out computer vision code

--- Divvy/HomeViewController.swift
+++ Divvy/HomeViewController.swift
@@ -38,131 +38,133 @@ UINavigationControllerDelegate {
         }
     }
     
-    func getItemInfo(lines: [String], lineIndex: Int, charIndex: Int) -> (String, Double) {
-        var foundPrice = false
-        var notItemName = false
-        var price = ""
-        var itemName = ""
-        let line = lines[lineIndex]
-        var i = 0
-        while i <= charIndex && !(foundPrice && notItemName) {
-            let currChar = line[line.startIndex.advancedBy(charIndex - i)]
-            //print(currChar)
-            if !foundPrice && currChar == " " {
-                foundPrice = true
-            } else if !foundPrice {
-                price = String(currChar) + price
-            }
-            if currChar == "." {
-                notItemName = true
-            } else if foundPrice {
-                itemName = String(currChar) + itemName
-            }
-            i += 1
-        }
-        if notItemName {
-            itemName = lines[lineIndex-1]
-        }
-        
-        price += String(line[line.startIndex.advancedBy(charIndex + 1)]) + String(line[line.startIndex.advancedBy(charIndex + 2)]) + String(line[line.startIndex.advancedBy(charIndex + 3)])
-        
-        print(itemName)
-        print(price)
-        
-        return (itemName.stringByTrimmingCharactersInSet(
-            NSCharacterSet.whitespaceAndNewlineCharacterSet()
-            ), Double(price)!)
-    }
+//    func getItemInfo(lines: [String], lineIndex: Int, charIndex: Int) -> (String, Double) {
+//        var foundPrice = false
+//        var notItemName = false
+//        var price = ""
+//        var itemName = ""
+//        let line = lines[lineIndex]
+//        var i = 0
+//        while i <= charIndex && !(foundPrice && notItemName) {
+//            let currChar = line[line.startIndex.advancedBy(charIndex - i)]
+//            //print(currChar)
+//            if !foundPrice && currChar == " " {
+//                foundPrice = true
+//            } else if !foundPrice {
+//                price = String(currChar) + price
+//            }
+//            if currChar == "." {
+//                notItemName = true
+//            } else if foundPrice {
+//                itemName = String(currChar) + itemName
+//            }
+//            i += 1
+//        }
+//        if notItemName {
+//            itemName = lines[lineIndex-1]
+//        }
+//        
+//        price += String(line[line.startIndex.advancedBy(charIndex + 1)]) + String(line[line.startIndex.advancedBy(charIndex + 2)]) + String(line[line.startIndex.advancedBy(charIndex + 3)])
+//        
+//        print(itemName)
+//        print(price)
+//        
+//        return (itemName.stringByTrimmingCharactersInSet(
+//            NSCharacterSet.whitespaceAndNewlineCharacterSet()
+//            ), Double(price)!)
+//    }
     
     func customFunction(image: UIImage) {
-        let ocrURL = "http://www.ocrwebservice.com/restservices/processDocument?gettext=true&tobw=true&outputformat=txt"
-        let data = (Model.sharedInstance.username + ":" + Model.sharedInstance.licenseCode).dataUsingEncoding(NSUTF8StringEncoding)
-        let headers = [
-            "Authorization": "Basic " + data!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0)),
-            "Content-Type": "application/json"
-        ]
+        //let ocrURL = "https://api.projectoxford.ai/vision/v1.0/ocr?"
+//        let headers = [
+//            "Ocp-Apim-Subscription-Key": Model.sharedInstance.microsoftAPIKey,
+//            "Content-Type": "application/octet-stream",
+//            "Accept": "application/octet-stream"
+//        ]
         
-        let testImage = UIImage(named: "publix")
-        let imageData = UIImagePNGRepresentation(testImage!)
-        
-        
-        Alamofire.upload(.POST, ocrURL, headers: headers, data: imageData!).responseJSON { response in
-            
-            debugPrint(response)
-            if let json = response.result.value {
-                print("JSON: \(json)")
-                let data = JSON(json)
-                let url = data["OutputFileUrl"].URL
-                do {
-                    let htmlString = try String(contentsOfURL: url!)
-                    print(htmlString)
-                    var items = [Item]()
-                    var lines = [String]()
-                    htmlString.enumerateLines {
-                        lines.append($0.line)
-                    }
-                    var blanksReached = false
-                    var index = 1
-                    while index < lines.count && !blanksReached {
-                        if lines[index - 1] == "" && lines[index] == "" {
-                            blanksReached = true
-                        } else if lines[index] != "" {
-                            index += 2
-                        } else {
-                            index += 1
-                        }
-                    }
-                    
-                    var maxLineLength = lines[index + 1].characters.count
-                    var maxLineIndex = index + 1
-                    for i in 2..<4 {
-                        let length = lines[index + i].characters.count
-                        if length > maxLineLength {
-                            maxLineLength = length
-                            maxLineIndex = index + i
-                        }
-                    }
-                    
-                    var lineIndex = -1
-                    var foundNum = false
-                    let maxLine = lines[maxLineIndex]
-                    while lineIndex >= -maxLineLength + 1 && !foundNum {
-                        if maxLine[maxLine.endIndex.advancedBy(lineIndex)] == "." {
-                            foundNum = true
-                        } else {
-                            lineIndex -= 1
-                        }
-                    }
-                    
-                    var charIndex = lines[maxLineIndex].characters.count + lineIndex - 1
-                    var i = maxLineIndex
-                    var moreItems = true
-                    while moreItems && i < lines.count {
-                        let line = lines[i]
-                        if line == "" {
-                            moreItems = false
-                        } else if line.characters.count > charIndex {
-                            let itemInfo = self.getItemInfo(lines, lineIndex: i, charIndex: charIndex)
-                            items.append(Item(itemName: itemInfo.0, price: itemInfo.1))
-                        }
-                        i += 1
-                    }
-                    print(items)
-                    Model.sharedInstance.items = items
-                    
-                    self.presentViewController(TransactionViewController(), animated: true, completion: nil)
-                    
-                } catch let error {
-                    print(error)
-                }
-            }
-        }
-
-        
-        
+//        let ocrURL = "http://www.ocrwebservice.com/restservices/processDocument?gettext=true&outputformat=txt&tobw=true"
+//        let str = (Model.sharedInstance.username + ":" + Model.sharedInstance.licenseCode).dataUsingEncoding(NSUTF8StringEncoding)
+//        let headers = [
+//            "Content-Type": "application/json",
+//            "Authorization" : "Basic " + (str?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0)))!
+//            
+//            ]
+//        let testImage = UIImage(named: "publix")
+//        let imageData = UIImagePNGRepresentation(image)
+//        
+//        Alamofire.upload(.POST, ocrURL, headers: headers, data: imageData!).responseJSON { response in
+//            
+//            debugPrint(response)
+//            if let json = response.result.value {
+//                let data = JSON(json)
+//                print(data)
+//                //print(self.extractStringsFromDictionary(json as! [String : AnyObject]))
+//                let url = data["OutputFileUrl"].URL
+//                do {
+//                    let htmlString = try String(contentsOfURL: url!)
+//                    print(htmlString)
+//                    var items = [Item]()
+//                    var lines = [String]()
+//                    htmlString.enumerateLines {
+//                        lines.append($0.line)
+//                    }
+//                    var blanksReached = false
+//                    var index = 1
+//                    while index < lines.count && !blanksReached {
+//                        if lines[index - 1] == "" && lines[index] == "" {
+//                            blanksReached = true
+//                        } else if lines[index] != "" {
+//                            index += 2
+//                        } else {
+//                            index += 1
+//                        }
+//                    }
+//                    
+//                    var maxLineLength = lines[index + 1].characters.count
+//                    var maxLineIndex = index + 1
+//                    for i in 2..<4 {
+//                        let length = lines[index + i].characters.count
+//                        if length > maxLineLength {
+//                            maxLineLength = length
+//                            maxLineIndex = index + i
+//                        }
+//                    }
+//                    
+//                    var lineIndex = -1
+//                    var foundNum = false
+//                    let maxLine = lines[maxLineIndex]
+//                    while lineIndex >= -maxLineLength + 1 && !foundNum {
+//                        if maxLine[maxLine.endIndex.advancedBy(lineIndex)] == "." {
+//                            foundNum = true
+//                        } else {
+//                            lineIndex -= 1
+//                        }
+//                    }
+//                    
+//                    var charIndex = lines[maxLineIndex].characters.count + lineIndex - 1
+//                    var i = maxLineIndex
+//                    var moreItems = true
+//                    while moreItems && i < lines.count {
+//                        let line = lines[i]
+//                        if line == "" {
+//                            moreItems = false
+//                        } else if line.characters.count > charIndex {
+//                            let itemInfo = self.getItemInfo(lines, lineIndex: i, charIndex: charIndex)
+//                            items.append(Item(itemName: itemInfo.0, price: itemInfo.1))
+//                        }
+//                        i += 1
+//                    }
+//                    print(items)
+//                    Model.sharedInstance.items = items
+//                    
+//                    self.presentViewController(TransactionViewController(), animated: true, completion: nil)
+//                    
+//                } catch let error {
+//                    print(error)
+//                }
+//            }
+            self.presentViewController(TransactionViewController(), animated: true, completion: nil)
     }
-    
-
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
