7875e09d61badeeb2c9d29e9d766d85864a81eb1
Rahul <rahul.nambiar@gatech.edu>
2016-09-24 22:54:16 -0400

    edit profile controller

--- Divvy/EditProfileViewController.swift
+++ Divvy/EditProfileViewController.swift
@@ -17,15 +17,33 @@ UINavigationControllerDelegate {
     var editDetailsView = EditProfileDetailsView(frame: CGRect.zero)
     
     func configureLabels() {
-        
+        editDetailsView.emailID.text = Model.sharedInstance.currUser?.email
+        editDetailsView.fName.text = Model.sharedInstance.currUser?.fName
+        editDetailsView.lName.text = Model.sharedInstance.currUser?.lName
     }
     
-    func configureButtons() {
-        
-    }
+//    func configureButtons() {
+//        self.editDetailsView.userPhoto.addTarget(self, action: #selector(doSomething), forControlEvents: .TouchUpInside)
+//    }
     
     func configureView() {
         
+        self.view.backgroundColor = DVUIConstants.colors.darkGrayBackground
+        //configureButtons()
+        configureLabels()
+        
+        let viewDict = [
+            "editDetView" : editDetailsView
+        ]
+        
+        self.view.prepareViewsForAutoLayout(viewDict)
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|[editDetView(==\(String(DVUIConstants.screenHeight * 0.75))]", views: viewDict))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|[editDetView]|", views: viewDict))
+    }
+    
+    func doSomething() {
+        print("hi")
     }
     
     override func viewWillAppear(animated: Bool) {
